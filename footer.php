1ee98172d1932600e6379ca5c45c8e450d5492ec
Raul Pena <nb-raul@live.com>
2016-06-06 19:47:52 -0500

    ;)

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'codergv' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'codergv' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'codergv' ), 'codergv', '<a href="http://www.codergv.org" rel="designer">CodeRGV Community</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'codergv' ), 'codergv', '<a href="http://www.codergv.org" rel="designer">CodeRGV Community ;)</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
